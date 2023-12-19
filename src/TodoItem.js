a5dc98ae2a73943e4edda3c2045a802a3ee1a122
surajsghodake <117455759+surajsghodake@users.noreply.github.com>
2023-12-19 13:51:05 +0530

    Initial Commit

---
+++ src/TodoItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function TodoItem({id, title, completed, toggleTodo, deleteTodo}) {
+  return (
+    <>
+        <li key={id}>
+            <label>
+                <input type='checkbox' checked={completed} onChange={e => toggleTodo(id, e.target.checked)} />
+                {title}
+            </label>
+            <button className='btn btn-danger' onClick={() => deleteTodo(id)}>Delete</button>
+        </li>
+    </>
+  )
+}
+
+export default TodoItem
